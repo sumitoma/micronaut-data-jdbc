f4f1d4c6bae836bd0ad852053186a5a503257758
Sumit Tomar <ssinghtomar.06@gmail.com>
2020-07-28 23:14:34 +0530

    added more tests

---
+++ src/main/java/micronaut/data/UserPageableRepository.java
@@ -0,0 +1,9 @@
+package micronaut.data;
+
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.PageableRepository;
+
+@JdbcRepository(dialect = Dialect.H2)
+public interface UserPageableRepository extends PageableRepository<User, String> {
+}
--- src/test/java/micronaut/data/MicronautDataJdbcTest.java
+++ src/test/java/micronaut/data/MicronautDataJdbcTest.java
@@ -1,5 +1,7 @@
 package micronaut.data;
 
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Assertions;
 import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @MicronautTest
 public class MicronautDataJdbcTest {
@@ -18,6 +21,8 @@ public class MicronautDataJdbcTest {
 
     @Inject
     UserRepository userRepository;
+    @Inject
+    UserPageableRepository userPageableRepository;
 
     @Test
     void testItWorks() {
@@ -33,6 +38,11 @@ public class MicronautDataJdbcTest {
     }
 
     @Test
+    void testCount(){
+       Assertions.assertEquals(2, userRepository.count());
+    }
+
+    @Test
     void testFindById(){
        Optional<User> user = userRepository.findById("sumit");
        if(user.isPresent()){
@@ -44,11 +54,38 @@ public class MicronautDataJdbcTest {
     }
 
     @Test
+    void testExistsById(){
+        Assertions.assertTrue(userRepository.existsById("sumit"));
+    }
+
+    @Test
     void testFindByFirstName(){
         AttributesDTO user = userRepository.findByFirstName("sumit");
         Assertions.assertNotNull(user);
         Assertions.assertEquals("sumit", user.getFirstName());
         Assertions.assertEquals(User.Status.ACTIVE, user.getStatus());
     }
+
+    @Test
+    void testFindAll(){
+        Iterable<User> userIterable = userRepository.findAll();
+        Assertions.assertNotNull(userIterable);
+        long totalCount = StreamSupport.stream(userIterable.spliterator(), false).count();
+        Assertions.assertEquals(2, totalCount);
+    }
+
+
+    //Pageable repository test
+    @Test
+    void testFindPage(){
+        Page<User> user = userPageableRepository.findAll(Pageable.from(0, 2));
+
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(2, user.getContent().size());
+        Assertions.assertEquals(1, user.getTotalPages());
+        Assertions.assertEquals(2, user.getTotalSize());
+
+    }
+
 
 }
